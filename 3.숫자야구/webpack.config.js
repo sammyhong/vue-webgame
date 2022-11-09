294aa06136cd32790b9ab11390582a084b7006d5
Sammy <samee1@navr.com>
2022-11-09 17:15:21 +0900

    숫자야구 게임

---
+++ 3.숫자야구/webpack.config.js
@@ -0,0 +1,24 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  resolve: { extensions: [".js", ".vue"] },
+  entry: {
+    app: path.join(__dirname, "main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+  output: {
+    filename: "app.js",
+    path: path.join(__dirname, "dist"),
+  },
+};
