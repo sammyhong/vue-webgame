b6c490823ae9740e53cca497b9b302d72c56e99e
sammyHong <samee1@naver.com>
2022-11-10 14:50:16 +0900

    반응속도체크

---
+++ 4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div id="screen" :class="state" @click="onClickScreen">{{ message }}</div>
+    <div v-if="result.length">
+      <div>
+        평균 시간 :
+        {{ average }}ms
+      </div>
+      <button @click="onReset">리셋</button>
+    </div>
+  </div>
+</template>
+<script>
+let startTime = 0;
+let endTime = 0;
+let timeOut = null;
+export default {
+  data() {
+    return {
+      result: [],
+      state: "waiting",
+      message: "클릭해서 시작하세요.",
+    };
+  },
+  computed: {
+    // 기존에 캐싱된 average가 실행되어 성능에 영향
+    average() {
+      return this.result.reduce((a, c) => a + c, 0) / this.result.length || 0;
+    },
+  },
+  methods: {
+    onReset() {
+      this.result = [];
+    },
+    onClickScreen() {
+      if (this.state === "waiting") {
+        this.state = "ready";
+        this.message = "초록색이 되면 클릭하세요.";
+
+        timeOut = setTimeout(() => {
+          this.state = "now";
+          this.message = "지금 클릭!";
+          startTime = new Date();
+        }, Math.floor(Math.random() * 1000) + 2000); // 대략 2~3초
+      } else if (this.state === "ready") {
+        clearTimeout(timeOut);
+        this.state = "waiting";
+        this.message = "너무 성급하시군요! 초록색이 된 후에 클릭하세요.";
+      } else if (this.state === "now") {
+        endTime = new Date();
+        this.state = "waiting";
+        this.message = "클릭해서 시작하세요.";
+        this.result.push(endTime - startTime);
+      }
+    },
+  },
+};
+</script>
+<style scope>
+#screen {
+  width: 300px;
+  height: 200px;
+  text-align: center;
+  user-select: none;
+}
+#screen.waiting {
+  background-color: aqua;
+}
+#screen.ready {
+  background-color: red;
+  color: white;
+}
+#screen.now {
+  background-color: greenyellow;
+}
+</style>
---
+++ 4.반응속도체크/main.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import ResponseCheck from "./ResponseCheck.vue";
+
+new Vue(ResponseCheck).$mount("#root");
