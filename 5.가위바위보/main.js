f518b1f79d921a89a28985b6703abde6c67ea705
sammyHong <samee1@naver.com>
2022-11-10 18:20:24 +0900

    가위바위보 게임

---
+++ 5.가위바위보/main.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import RockScissorsPaper from "./RockScissorsPaper.vue";
+
+new Vue(RockScissorsPaper).$mount("#root");
---
+++ 5.가위바위보/webpack.config.js
@@ -0,0 +1,28 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  resolve: { extensions: [".js", ".vue"] },
+  entry: {
+    app: path.join(__dirname, "main.js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+  output: {
+    filename: "app.js",
+    path: path.join(__dirname, "dist"),
+  },
+};
